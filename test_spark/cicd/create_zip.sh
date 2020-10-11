8284cb90ff4c6c7307f141ff9a31de7291f86b70
PietroGuardati <pietroguardati@gmail.com>
2020-10-11 22:09:41 +0200

    moved shell scripts into cicd

--- test_spark/cicd/create_zip.sh
+++ test_spark/cicd/create_zip.sh
@@ -2,3 +2,7 @@
 echo "\nCreating zip archive of python module.."
 zip -r test_spark.zip test_spark
 conda-pack -n test_spark -o environment.tar.gz
+
+echo "\nReset logs folder"
+mkdir log/events
+mkdir log/nodes
