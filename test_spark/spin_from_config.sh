953e27c4b78347ec2f977d5f26eb3d0f5ad58240
PietroGuardati <pietroguardati@gmail.com>
2020-10-11 21:52:43 +0200

    finalised local configuration

--- test_spark/spin_from_config.sh
+++ test_spark/spin_from_config.sh
@@ -1,6 +1,4 @@
 # ---  inside the cluster
-zip -r test_spark.zip test_spark
-
 echo "\nImport spark environment configuration"
 export SPARK_HOME=/usr/local/Cellar/apache-spark/3.0.1/libexec/
 cp conf/spark-env.sh $SPARK_HOME/conf/spark-env.sh
@@ -18,7 +16,6 @@ echo "\nCopying env. variables and spinning master and slaves:"
 $SPARK_HOME/sbin/start-master.sh
 $SPARK_HOME/sbin/start-slave.sh spark://localhost:7077
 jps
-
 
 echo "\nSubmitting file using configuration file"
 $SPARK_HOME/bin/spark-submit \
