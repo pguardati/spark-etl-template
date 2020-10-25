b83343062e63b99ecdd39ea5fdb3feff475ae290
PietroGuardati <pietroguardati@gmail.com>
2020-10-25 02:22:53 +0200

    refactor: minor modification to main file and environment

--- test_spark/src/library.py
+++ test_spark/src/library.py
@@ -1,8 +1,4 @@
-import pandas as pd
-
-
 def test_fun(spark, logger):
     logger.info("Creating a dataframes..")
-    df_pandas = pd.DataFrame([[1, 2], [3, 4]])
     df_spark = spark.createDataFrame([('Alice', 1), ('Me', 2)])
     return df_spark
