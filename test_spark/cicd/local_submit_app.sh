229960abbb12894ad541d3faf8841756a05b7805
PietroGuardati <pietroguardati@gmail.com>
2020-10-25 02:23:51 +0200

    feat: add script to deploy local spark application

--- test_spark/cicd/local_submit_app.sh
+++ test_spark/cicd/local_submit_app.sh
@@ -1,6 +1,11 @@
+MODULE_NAME="test_spark"
+
 echo "\nImporting spark environment configuration.."
 echo "Spark home: $SPARK_HOME"
 cp ../config/spark-env-local.sh $SPARK_HOME/conf/spark-env.sh
+
+echo "\nPackaging application.."
+sh $MODULE_NAME/cicd/package_app.sh
 
 echo "\nKilling master and slave.."
 $SPARK_HOME/sbin/stop-master.sh
@@ -10,7 +15,7 @@ echo "\nSpinning master and slaves:"
 $SPARK_HOME/sbin/start-master.sh
 $SPARK_HOME/sbin/start-slave.sh spark://localhost:7077
 
-echo "\nSubmitting unittests.."
+echo "\nSubmitting app.."
 $SPARK_HOME/bin/spark-submit \
 --deploy-mode client \
 --master spark://localhost:7077 \
@@ -18,4 +23,6 @@ $SPARK_HOME/bin/spark-submit \
 --properties-file test_spark/config/spark-defaults.conf \
 --py-files archive/test_spark.zip \
 --archives archive/environment.zip#environment \
-test_spark/script/main.py > log/events/app_response.txt
+test_spark/script/main.py
+
+echo "\nApplication Completed"
---
+++ test_spark/cicd/package_app.sh
@@ -0,0 +1,14 @@
+MODULE_NAME="test_spark"
+DIR_PACKAGING="archive"
+
+# --- outside the cluster
+echo "\Creating archive folder"
+mkdir $DIR_PACKAGING
+
+echo "\nCreating zip archive of python module and environment.."
+conda env export --no-builds --from-history | grep -v "^prefix: "> $MODULE_NAME/environment.yml
+conda-pack -o $DIR_PACKAGING/environment.zip -n $MODULE_NAME --format zip
+zip -r $DIR_PACKAGING/$MODULE_NAME.zip $MODULE_NAME
+
+echo "\nCreate log folder"
+mkdir log/
